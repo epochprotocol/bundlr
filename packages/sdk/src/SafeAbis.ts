c19ff7bb5c785d1c992f87f6903efb4f65922498
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2024-02-08 19:30:32 +0530

    Added abi encoding to multisend call

--- packages/sdk/src/SafeAbis.ts
+++ packages/sdk/src/SafeAbis.ts
@@ -1137,3 +1137,13 @@ export const ERC20_ABI = [
     type: "function",
   },
 ];
+export const MultisendAbi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    inputs: [{ internalType: "bytes", name: "transactions", type: "bytes" }],
+    name: "multiSend",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
